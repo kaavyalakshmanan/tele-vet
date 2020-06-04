790e51b3875884049b943af8cb278a7ca961fb4f
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-06-04 14:54:41 -0700

    Added vet registration form

--- src/components/VetSignUp.js
+++ src/components/VetSignUp.js
@@ -6,29 +6,65 @@ class VetSignUp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          email: "",
-          password: ""
-        };
+            user_name: "",
+            clinic_name: "",
+            address: "",
+            password: "",
+            email: "",
+            phone: ""
+          };
       }
     render() {
       
-        const { email, password } = this.state;
+        const { user_name, clinic_name, address, email, password, phone } = this.state;
         return (
           <form onSubmit={this.handleSubmit}>
+            <label htmlFor="user_name">Name</label>
+            <input
+              name="user_name"
+              type="text"
+              placeholder="Enter your full name"
+              value={user_name}
+              onChange={this.handleChange}
+            />
+             <label htmlFor="clinic_name">Clinic Name</label>
+            <input
+              name="clinic_name"
+              type="text"
+              placeholder="Enter your clinic name"
+              value={clinic_name}
+              onChange={this.handleChange}
+            />
+             <label htmlFor="address">Clinic Address</label>
+            <input
+              name="address"
+              type="text"
+              placeholder="Enter your clinic name"
+              value={address}
+              onChange={this.handleChange}
+            />
             <label htmlFor="email">Email</label>
             <input
               name="email"
-              type="text"
+              type="email"
               placeholder="Enter your email"
               value={email}
               onChange={this.handleChange}
             />
             <label htmlFor="email">Password</label>
-            <input
+              <input
               name="password"
               type="password"
               placeholder="Enter your password"
               value={password}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="phone">Phone Number</label>
+              <input
+              name="phone"
+              type="password"
+              placeholder="Enter your phone number"
+              value={phone}
               onChange={this.handleChange}
             />
             <button type="submit">Login</button>
