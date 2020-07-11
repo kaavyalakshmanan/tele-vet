b251ae8397b9ee0870c0fc8845f7643d4fe5ffa0
amygeorge <ageorge@alumni.ubc.ca>
2020-07-11 04:57:03 -0700

    Maps APIs
    Signed-off-by: amygeorge <ageorge@alumni.ubc.ca>

--- api/app.js
+++ api/app.js
@@ -36,6 +36,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://maps.googleapis.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/users', usersRouter);
@@ -56,5 +63,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- src/components/maps/FindVet.jsx
+++ src/components/maps/FindVet.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Maps from "./Maps";
 import VetCards from "./VetCards";
+import axios from "axios"
 
 
-function FindVet() {
+axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&&keyword=vet&key=AIzaSyBREwvARS3lmrahtK3OFrNG2Ev3QUm1Spw')
+  .then(function (response) {
+    // handle success
+    console.log("response", response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+
+
+export default function FindVet() {
 
     return (
         <div id="wrapper">
+
             <div id="map">
                 <Maps/>
             </div>
@@ -18,5 +35,3 @@ function FindVet() {
     );
 
 }
-
-export default FindVet;
