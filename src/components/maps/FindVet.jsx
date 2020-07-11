1aca5afccec720222af39f3a1063f9540456a66f
amygeorge <ageorge@alumni.ubc.ca>
2020-07-11 05:42:16 -0700

    Connected to maps API
    Signed-off-by: amygeorge <ageorge@alumni.ubc.ca>

--- src/components/maps/FindVet.jsx
+++ src/components/maps/FindVet.jsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Maps from "./Maps";
 import VetCards from "./VetCards";
 import axios from "axios"
 
 
-axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&&keyword=vet&key=AIzaSyBREwvARS3lmrahtK3OFrNG2Ev3QUm1Spw')
-  .then(function (response) {
-    // handle success
-    console.log("response", response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
-
+const proxy = "https://cors-anywhere.herokuapp.com/";
+const target = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=vets+in+Vancouver&key=AIzaSyBREwvARS3lmrahtK3OFrNG2Ev3QUm1Spw";
 
 
 export default function FindVet() {
 
+    const [data, setData] = useState([]);
+    useEffect (() => {
+
+      axios.get(proxy + target)
+            .then(res => {
+                console.log("hello result", res.data.results);
+                // setData(res.data.results);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            })
+    }, [])
+
+    console.log("wanted", data)
     return (
         <div id="wrapper">
 
             <div id="map">
-                <Maps/>
+                <Maps
+                     data={data}
+                />
+
             </div>
             <div id="card">
-                <VetCards/>
+                <VetCards
+                    data={data}
+                    />
             </div>
         </div>
 
