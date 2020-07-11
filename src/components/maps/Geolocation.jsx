eeadd4987bb00b55ec0020872091634a85815587
amygeorge <ageorge@alumni.ubc.ca>
2020-07-11 00:41:34 -0700

    revamping maps
    Signed-off-by: amygeorge <ageorge@alumni.ubc.ca>

---
+++ src/components/maps/Geolocation.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+
+{console.log("LOCATION!", navigator.geolocation.getCurrentPosition(function (position) {
+                        let pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        console.log(pos)
+
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+let map, infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 49.257803, lng: -123.119299},
+        zoom: 12
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: Please allow access to location.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+
+
--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
 import vetData from "../../resources/vet_data";
 
 
 //________________________________________________________
 const mapStyles = {
-    width: '45%',
+    width: '50%',
     height: '70%',
     position: 'relative'
 
@@ -34,25 +34,100 @@ class Maps extends React.Component {
                 lng: vet.geometry.location.lng
             }}
 
-           onClick={() => console.log("You clicked me!")}/>
+                           onClick={() => console.log("You clicked me!")}/>
         })
     }
 
+    initMap = () => {
+        console.log("async", this.initialMap())
+        let position = this.initialMap()
+        let pos;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                console.log("success", pos)
+            })
+        } else {
+            pos = {lat: 49.257803, lng: -123.119299}
+
+            console.log("local", pos)
+        }
+        console.log("now", pos)
+        return <Map id="map_loader"
+                    google={this.props.google}
+                    zoom={12}
+                    style={mapStyles}
+                    initialCenter={position}
+
+        />
+
+    }
+
+    initialMap = async () => {
+        async function getPos() {
+            let pos;
+
+            // if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                console.log("geo", pos)
+
+                return pos
+            })
+            // } else {
+            //     pos = {lat: 49.257803, lng: -123.119299}
+            // }
+            // console.log("now", pos)
+            // return pos
+        }
+
+        console.log("lost", getPos())
+        return await getPos()
+    }
+
+
+    setUserCenter = () => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            console.log(pos)
+            return pos
+        })
+    }
 
     render() {
         return (
             <div id='mapDisplay'>
                 <h1>Find a vet to help your pet! </h1>
 
-                <Map id="map_loader"
-                    google={this.props.google}
-                    zoom={12}
-                     style={mapStyles}
-                    initialCenter={{lat: 49.257803, lng: -123.119299}}
-                >
-                    {this.displayMarkers()}
+                {/*<Map id="map_loader"*/}
+                {/*     google={this.props.google}*/}
+                {/*     zoom={12}*/}
+                {/*     style={mapStyles}*/}
+                {/*     initialCenter={{lat: 49.257803, lng: -123.119299}}*/}
+                {/*>*/}
+                <div id="map">
+                    {this.initMap()}
+                    {/* {this.displayMarkers()}*/}
+                    {/*/!*{console.log("LOCATION!", navigator.geolocation.getCurrentPosition(function (position) {*!/*/}
+                    {/*/!*    let pos = {*!/*/}
+                    {/*/!*        lat: position.coords.latitude,*!/*/}
+                    {/*/!*        lng: position.coords.longitude*!/*/}
+                    {/*/!*    };*!/*/}
+                    {/*/!*    console.log(pos)*!/*/}
+                    {/*/!*}))}*!/*/}
+                    {/*// {console.log(this.setUserCenter())}*/}
 
-                </Map>
+                </div>
+                {/*</Map>*/}
 
             </div>
 
