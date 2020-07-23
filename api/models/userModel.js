92c189ad6ad4e2f84868e2bff57a8dace40f4f60
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-07-23 16:17:48 -0700

    Added endpoint to get user by ID

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,27 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    // isAuthenticated: {type: Boolean},
-    // isFetching: {type: Boolean},
-    // didInvalidate: {type: Boolean},
-    // email: {type: String, required: [true, "Password is required"], unique: true},
-    // username: {type: String, required: [true, "Username is required"], unique: true},
-    // password: {type: String, required: [true, "Password is required"]},
-    // profilePicture: {type: String},
-    // lastUpdate: {type: Number},
-    // images: {
-    //     list: [{}]
-    // },
-    // appointments: {
-    //     list: [{}]
-    // },
-    // messages: {
-    //     contactList: [{}]
-    // },
-    // documents: {
-    //     list: [{}]
-    // }
-    //videoConferenceId: {type: String}
 
     name: {
         type: String,
@@ -60,18 +39,10 @@ const userSchema = new Schema({
     lastUpdate: {
         type: Number
     },
-    images: {
-        list: [{}]
-    },
-    appointments: {
-        list: [{}]
-    },
-    messages: {
-        contactList: [{}]
-    },
-    documents: {
-        list: [{}]
-    }
+    images: [{}],
+    appointments: [{}],
+    messages: [{}],
+    documents: [{}]
     //videoConferenceId: {type: String}
 });
 
