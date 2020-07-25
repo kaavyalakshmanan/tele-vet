7bb8e893bb25e3a9de2d8bfe23225d8b4391d474
Nick Peterson <nick.peterson@alumni.ubc.ca>
2020-07-25 09:25:51 -0700

    Restore deployment server settings

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
 
 app.use('/vets', vetsRouter);
 app.use('/users', usersRouter);
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
