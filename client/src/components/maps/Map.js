c2fcd4e3af1447d039fbb20c4c375bf7ac026c10
Nick Peterson <nick.peterson@alumni.ubc.ca>
2020-07-24 22:02:30 -0700

    fix broken import in Map.js

--- client/src/components/maps/Map.js
+++ client/src/components/maps/Map.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import isEmpty from '../../../node_modules/lodash.isempty';
+import isEmpty from 'lodash.isempty';
 import vetData from "../../resources/vet_data";
 import GoogleMap from './GoogleMap';
 
