1cb62548d5b07eb3d03bfd142cb6e97bfec1dd45
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-07-24 22:40:19 -0700

    Added additional heroku script

--- app.js
+++ app.js
@@ -70,6 +70,16 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Serve static assets if we are in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 
 module.exports = app;
