0d89cfa6a2c7a5e4f4373258e4028f2151f510da
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-07-27 23:04:27 -0700

    Updated user model to include name

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
     isAuthenticated: {type: Boolean},
     isFetching: {type: Boolean},
     didInvalidate: {type: Boolean},
+    name: {type: String, required: [true, "Name is required"]},
     email: {type: String, required: [true, "Password is required"], unique: true},
     username: {type: String, required: [true, "Username is required"], unique: true},
     password: {type: String, required: [true, "Password is required"]},
