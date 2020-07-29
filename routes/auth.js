e43382fffd7e49220029990634c0cb01996be832
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-07-29 13:23:47 -0700

    Endpoint to update existing user's data

--- routes/auth.js
+++ routes/auth.js
@@ -68,6 +68,64 @@ router.post('/', (req, res) => {
             .then(user => res.json(user));
     });
 
+/* @route  PUT /auth/user
+   @desc   Put request to update an existing user
+   @access Private */
+   
+router.put('/user', auth, (req, res) => {
+    User.findById(req.user.id, (err, updatedUser) => {
+        if (!updatedUser) res.status(404).json({msg: "User not found"})
+        else {
+            updatedUser.name = req.body.name;
+            updatedUser.email = req.body.email;
+            updatedUser.username = req.body.username;
+            updatedUser.password = req.body.password;
+            updatedUser.profilePicture = req.body.profilePicture;
+            updatedUser.lastUpdate = req.body.lastUpdate;
+            updatedUser.images = req.body.images;
+            updatedUser.appointments = req.body.appointments;
+            updatedUser.messages = req.body.messages;
+            updatedUser.documents = req.body.documents;
+
+               // Create salt and hash
+               bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(updatedUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    updatedUser.password = hash;
+                    updatedUser.save()
+                        .then(user => {
+
+                            jwt.sign(
+                                {id: user.id},
+                                config.get('jwtSecret'),
+                                {expiresIn: 3600},
+                                (err, token) => {
+                                    if (err) throw err;
+                                    res.json({
+                                        token,
+                                        user: {
+                                            id: user.id,
+                                            name: user.name,
+                                            email: user.email,
+                                            username: user.username
+                                        }
+                                    });
+                                });
+                        });
+                });
+            });
+
+            updatedUser.save()
+                .then(updatedUser => {
+                    res.status(200).json({msg: "User has been successfully updated!"})
+                })
+                .catch(err => {
+                    res.status(404).json({msg: "There was an issue updating the user"})
+                })
+
+        }
+    })
+})
 
 
 
