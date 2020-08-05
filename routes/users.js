a2e78b766f59b12ce3f3d35d0e09b38c5eda4dfa
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-08-05 08:44:29 -0700

    Cleaned up routes/users and added credits/comments

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,54 @@
-// import {User} from '../models/userModel';
+/* REGISTERING A NEW USER
+   Credits to help us understand how to use JWT and bcryptjs: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
+*/
+
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
-// REGISTERING A NEW USER
-
 // User Model
 const User = require('../models/userModel');
 
-// @route   POST /users
-// @desc    Register a new user
-// @access  Public
+/* @route   POST /users
+   @desc    Register a new user
+   @access  Public */
+
 router.post('/', (req, res) => {
     const {email, username, password} = req.body;
 
-    // Simple validation
+    // If user does not input username, email, or password, send 400 response
     if (!username || !email || !password) {
-        // Bad request, user did not send correct info
         return res.status(400).json({msg: 'Please enter all fields'});
     }
 
-    // Check for existing user
+    // If user is already registered, send 400 response 
     User.findOne({username})
         .then(user => {
             if (user) return res.status(400).json({msg: 'User already exists'});
 
+            // Create new user object with req.body that's passed in from axios post request
             const newUser = new User(req.body);
 
-            // Create salt and hash
+            // Salt & hash password to make more secure
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err;
+                    // Set user's password to hashed password
                     newUser.password = hash;
+                    // Dave user to database
                     newUser.save()
                         .then(user => {
-
+                            // Sign user in using JWT token
                             jwt.sign(
                                 {id: user.id},
                                 config.get('jwtSecret'),
+                                // Keep user logged in for 1 hour
                                 {expiresIn: 3600},
                                 (err, token) => {
                                     if (err) throw err;
+                                    // Send JSON response with relevant user info
                                     res.json({
                                         token,
                                         user: {
