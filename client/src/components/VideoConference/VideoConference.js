09b93d0a096118e26101123e10747a116b504590
franco <francoischarette@outlook.com>
2020-08-05 19:15:44 -0700

    cleaned videoConference and CSS

--- client/src/components/VideoConference/VideoConference.js
+++ client/src/components/VideoConference/VideoConference.js
@@ -1,26 +1,16 @@
 import React, { useEffect } from 'react';
 const { v4: uuidv4 } = require('uuid');
 
-
-
 function JitsiMeetComponent() {
-    // const [loading, setLoading] = useState(true);
     const containerStyle = {
-        // width: '800px',
-        // height: '400px',
         width: "100%",
         height: "100%",
     };
 
-    // const id = uuidv4();
-
     const jitsiContainerStyle = {
         display: "block",
-            // (loading ? 'none' : 'block'),
         width: '100%',
         height: '100%',
-        // width: '800px',
-        // height: '400px',
     }
 
     function startConference() {
@@ -30,7 +20,6 @@ function JitsiMeetComponent() {
             const options = {
                 roomName: id,
                 height: 800,
-                // security: Need to find this option in the API and implement itS
                 parentNode: document.getElementById('jitsi-container'),
                 interfaceConfigOverwrite: {
                     filmStripOnly: false,
@@ -44,8 +33,7 @@ function JitsiMeetComponent() {
             const api = new window.JitsiMeetExternalAPI(domain, options);
             api.addEventListener('videoConferenceJoined', () => {
                 console.log('Local User Joined');
-                // setLoading(false);
-                api.executeCommand('displayName', 'Dr. Shawarma');
+                api.executeCommand('displayName');
             });
         } catch (error) {
             console.error('Failed to load Jitsi API', error);
@@ -53,7 +41,6 @@ function JitsiMeetComponent() {
     }
 
     useEffect(() => {
-        // verify the JitsiMeetExternalAPI constructor is added to the global..
         if (window.JitsiMeetExternalAPI) startConference();
         else alert('Jitsi Meet API script not loaded');
     }, []);
@@ -62,7 +49,6 @@ function JitsiMeetComponent() {
         <div
             style={containerStyle}
         >
-            {/*{loading && <ProgressComponent />}*/}
             <div
                 id="jitsi-container"
                 style={jitsiContainerStyle}
@@ -73,10 +59,9 @@ function JitsiMeetComponent() {
 
 export default JitsiMeetComponent;
 
-
-// https://jitsi.github.io/handbook/docs/dev-guide/dev-guide-iframe
-//https://github.com/jitsi/lib-jitsi-meet/blob/master/doc/API.md#installation
-//https://gitee.com/huangranrumeng/jitsi-meet/raw/a9bdde193da5d57cbbd4e8c89afebe6de71544a5/doc/api.md
-//https://meetrix.io/blog/webrtc/integrate-jitsi-meet-to-react-app.html
-//https://jitsi.org/api/
-//https://github.com/gatteo/react-jitsi/tree/master/example
+//Code for building this component was taken from these sources:
+// - https://jitsi.github.io/handbook/docs/dev-guide/dev-guide-iframe
+// - https://github.com/jitsi/lib-jitsi-meet/blob/master/doc/API.md#installation
+// - https://gitee.com/huangranrumeng/jitsi-meet/raw/a9bdde193da5d57cbbd4e8c89afebe6de71544a5/doc/api.md
+// - https://meetrix.io/blog/webrtc/integrate-jitsi-meet-to-react-app.html
+// - https://jitsi.org/api/
