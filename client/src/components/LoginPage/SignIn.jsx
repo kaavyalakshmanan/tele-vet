58f9a309a71b6a9c3be75ec4d62d76d192d9e3dd
franco <francoischarette@outlook.com>
2020-08-06 20:11:00 -0700

    fix typos

--- client/src/components/LoginPage/SignIn.jsx
+++ client/src/components/LoginPage/SignIn.jsx
@@ -22,7 +22,7 @@ function Copyright() {
   return (
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
+        <Link color="inherit" href="https://tele-vet.herokuapp.com/">
           TeleVet
         </Link>{' '}
         {new Date().getFullYear()}
