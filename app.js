f9cd766f9471ef56a209a3e37ed155123dd67de7
Nick Peterson <nick.peterson@alumni.ubc.ca>
2020-08-06 22:41:52 -0700

    Remove static server

--- app.js
+++ app.js
@@ -84,13 +84,13 @@ app.use(function(err, req, res, next) {
 });
 
 // Serve static third-party-assets-material-ui if we are in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//if (process.env.NODE_ENV === 'production') {
+//  // Set static folder
+//  app.use(express.static('client/build'));
+//
+//  app.get('*', (req, res) => {
+//      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//  });
+//}
 
 module.exports = app;
